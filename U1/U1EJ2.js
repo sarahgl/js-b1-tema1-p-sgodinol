17650440c900c147cc66e6b22d92bc12c324b1bf
sarahgl <sarah.llebres@gmail.com>
2025-12-08 22:58:58 +0100

    unidad1_ejercicio2

--- U1/U1EJ2.js
+++ U1/U1EJ2.js
@@ -1,72 +1,75 @@
 //T1: Casos específicos de funciones / Casos específics de funcions
 //U1: Métodos map y filter / Mètodes map i filter
 /** 
-	-- CASTELLANO --
-	EJERCICIO 1.1.2 ENUNCIADO:
-	Dado el Array planets, compuesto de objetos literales de JS que representan a los planetas del sistema solar, 
-	se nos pide, utilizando el método filter, definir en una constante a la que llamaremos [result], un nuevo Array 
-	en el cual solamente tendremos planetas con anillos (los definidos por la propiedad hasRings = true)
+  -- CASTELLANO --
+  EJERCICIO 1.1.2 ENUNCIADO:
+  Dado el Array planets, compuesto de objetos literales de JS que representan a los planetas del sistema solar, 
+  se nos pide, utilizando el método filter, definir en una constante a la que llamaremos [result], un nuevo Array 
+  en el cual solamente tendremos planetas con anillos (los definidos por la propiedad hasRings = true)
 	
-	-- CATALÀ --
-	EXERCICI 1.1.2 ENUNCIAT:
-	Donat l'Array planets, compost d'objectes literals de JS que representen els planetes del sistema solar,
-	se'ns demana, emprant el mètode filter, definir en una constant a la qual anomenarem [result], un nou Array
-	en el qual únicament tindrem planetes amb anells (els definits per la propietat hasRings = true)
+  -- CATALÀ --
+  EXERCICI 1.1.2 ENUNCIAT:
+  Donat l'Array planets, compost d'objectes literals de JS que representen els planetes del sistema solar,
+  se'ns demana, emprant el mètode filter, definir en una constant a la qual anomenarem [result], un nou Array
+  en el qual únicament tindrem planetes amb anells (els definits per la propietat hasRings = true)
 */
 //Do not modify this code:
 let planets = [
-        {
-            name: "Mercury",
-            distanceToSun: 58344000,
-            hasRings: false,
-            diameter: 4878
-        },
-        {
-            name: "Venus",
-            distanceToSun: 107712000,
-            hasRings: false,
-            diameter: 12100
-        },
-        {
-            name: "Earth",
-            distanceToSun: 149600000,
-            hasRings: false,
-            diameter: 12756
-        },
-        {
-            name: "Mars",
-            distanceToSun: 227392000,
-            hasRings: false,
-            diameter: 6787
-        },
-        {
-            name: "Jupiter",
-            distanceToSun: 777920000,
-            hasRings: true,
-            diameter: 142984
-        },
-        {
-            name: "Saturn",
-            distanceToSun: 1427184000,
-            hasRings: true,
-            diameter: 120536
-        },
-        {
-            name: "Uranus",
-            distanceToSun: 2600000000,
-            hasRings: true,
-            diameter: 51108
-        },
-        {
-            name: "Neptune",
-            distanceToSun: 4300000000,
-            hasRings: true,
-            diameter: 49538
-        }
-    ]
+  {
+    name: "Mercury",
+    distanceToSun: 58344000,
+    hasRings: false,
+    diameter: 4878
+  },
+  {
+    name: "Venus",
+    distanceToSun: 107712000,
+    hasRings: false,
+    diameter: 12100
+  },
+  {
+    name: "Earth",
+    distanceToSun: 149600000,
+    hasRings: false,
+    diameter: 12756
+  },
+  {
+    name: "Mars",
+    distanceToSun: 227392000,
+    hasRings: false,
+    diameter: 6787
+  },
+  {
+    name: "Jupiter",
+    distanceToSun: 777920000,
+    hasRings: true,
+    diameter: 142984
+  },
+  {
+    name: "Saturn",
+    distanceToSun: 1427184000,
+    hasRings: true,
+    diameter: 120536
+  },
+  {
+    name: "Uranus",
+    distanceToSun: 2600000000,
+    hasRings: true,
+    diameter: 51108
+  },
+  {
+    name: "Neptune",
+    distanceToSun: 4300000000,
+    hasRings: true,
+    diameter: 49538
+  }
+]
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
-
-
+const result = planets.filter(planet => {
+  if (planet.hasRings == true) {
+    return [planet];
+  }
+});
 
 /**
  * TEST
@@ -75,5 +78,5 @@ let planets = [
  * please comment or remove the following lines
  */
 export function tests() {
-    return result;
+  return result;
 }
