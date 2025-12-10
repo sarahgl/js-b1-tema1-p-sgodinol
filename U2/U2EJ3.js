af95f823e40c850c4b1dc889d20293d2f31288e4
sarahgl <sarah.llebres@gmail.com>
2025-12-10 17:32:57 +0100

    unidad2_ejercicio3

--- U2/U2EJ3.js
+++ U2/U2EJ3.js
@@ -1,25 +1,27 @@
 //T1: Casos específicos de funciones / Casos específics de funcions
 //U2: Recursividad / Recursivitat
 /** 
-	-- CASTELLANO --
-	EJERCICIO 1.2.3 ENUNCIADO:
-	Define el cuerpo de una función recursiva denominada [oddNumbers] que recibiba un número y un Array vacío como parámetro y rellene este Array 
-	recibido como parámetro con todos los números impares que hay entre cero y el número recibido (con este incluido) como parámetro. 
-	Por ejemplo siendo myArray un Array vacío, la llamada a la función oddNumbers(7, myArray) devolvería myArray con los valores [7, 5, 3, 1] 
-	-- CATALÀ --
-	EXERCICI 1.2.3 ENUNCIAT:
-	Defineix el cos d'una funció recursiva anomenada [oddNumbers] que rebi un nombre i un Array buit com a paràmetre i empleni aquest Array
-	rebut com a paràmetre amb tots els números senars que hi ha entre zero i el nombre rebut (amb aquest inclòs) com a paràmetre.
-	Per exemple sent myArray un Array buit, la crida a la funció oddNumbers(7, myArray) tornaria myArray amb els valors [7, 5, 3, 1]
+  -- CASTELLANO --
+  EJERCICIO 1.2.3 ENUNCIADO:
+  Define el cuerpo de una función recursiva denominada [oddNumbers] que recibiba un número y un Array vacío como parámetro y rellene este Array 
+  recibido como parámetro con todos los números impares que hay entre cero y el número recibido (con este incluido) como parámetro. 
+  Por ejemplo siendo myArray un Array vacío, la llamada a la función oddNumbers(7, myArray) devolvería myArray con los valores [7, 5, 3, 1] 
+	
+  -- CATALÀ --
+  EXERCICI 1.2.3 ENUNCIAT:
+  Defineix el cos d'una funció recursiva anomenada [oddNumbers] que rebi un nombre i un Array buit com a paràmetre i empleni aquest Array
+  rebut com a paràmetre amb tots els números senars que hi ha entre zero i el nombre rebut (amb aquest inclòs) com a paràmetre.
+  Per exemple sent myArray un Array buit, la crida a la funció oddNumbers(7, myArray) tornaria myArray amb els valors [7, 5, 3, 1]
 */
 //Do not modify this code:
 let myArray = new Array();
 oddNumbers(7, myArray);
-
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
-
-
-
+function oddNumbers(number, array) {
+  if (number <= 0) return;
+  (number % 2 === 1) && array.push(number);
+  return oddNumbers(number - 1, array);
+}
 
 /**
  * TEST
@@ -28,5 +30,5 @@ oddNumbers(7, myArray);
  * please comment or remove the following lines
  */
 export function tests() {
-    return oddNumbers;
+  return oddNumbers;
 }
